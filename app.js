68a167a5ce53e89a9878ee6965e3df565e3e3e24
tkurosaki1 <tharunsainayuni@gmail.com>
2022-09-18 13:25:25 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -1,63 +1,45 @@
-// // getComputerChoice function
-
-// function getComputerChoice(){
-//  var compchoice = Math.floor(Math.random() * 3)
-
-// var computerSelection = "random"
-
-// if (compchoice === 0){
-//     computerSelection = "Rock"
-// } else if(compchoice === 1){
-//     computerSelection = "Paper"
-// } else{
-//     computerSelection = "Scissors"
-// }
-
-// console.log(computerSelection)
-// }
-
-
-// // playGame function
-
-// function playGame(computerSelection,playerSelection){
+// // playRound function
+// function playRound(playerSelection, computerSelection) {
 // if (computerSelection === "Rock" && playerSelection === "Paper"){
-// console.log("You have chosen Paper---You Wonn!!") 
+// return "You have chosen Paper---You Wonn!!" 
 // }else if (computerSelection === "Rock" && playerSelection === "Scissors"){
-// console.log("You have chosen Scissors---You Lost!!")
+// return "You have chosen Scissors---You Lost!!"
 // }else if (computerSelection === "Rock" && playerSelection === "Rock" || computerSelection === "Scissors" && playerSelection === "Scissors" || computerSelection === "Paper" && playerSelection === "Paper"){
-// console.log(`You have chosen ${playerSelection} --- you Tie!!`)
+// return `You have chosen ${playerSelection} --- you Tie!!`
 // }else if(computerSelection === "Paper" && playerSelection === "Rock"){
-// console.log("You have chosen Rock --- you Lost!!!")
+// return "You have chosen Rock --- you Lost!!!"
 // }else if(computerSelection === "Paper" && playerSelection === "Scissors"){
-// console.log("You have chosen Scissors --- You Won")    
+// return "You have chosen Scissors --- You Won"    
 // }else if(computerSelection === "Scissors" && playerSelection === "Paper"){
-// console.log("You have chosen Paper --- You Lost!!!")
+// return "You have chosen Paper --- You Lost!!!"
 // }else if(computerSelection === "Scissors" && playerSelection === "Rock"){
-// console.log("You chose Rock --- You won!!!")
+// return "You chose Rock --- You won!!!"
+
 // }
 // }
 
-
-
+// playRound function 2.0
 
 function playRound(playerSelection, computerSelection) {
-if (computerSelection === "Rock" && playerSelection === "Paper"){
-return "You have chosen Paper---You Wonn!!" 
-}else if (computerSelection === "Rock" && playerSelection === "Scissors"){
-return "You have chosen Scissors---You Lost!!"
-}else if (computerSelection === "Rock" && playerSelection === "Rock" || computerSelection === "Scissors" && playerSelection === "Scissors" || computerSelection === "Paper" && playerSelection === "Paper"){
-return `You have chosen ${playerSelection} --- you Tie!!`
-}else if(computerSelection === "Paper" && playerSelection === "Rock"){
-return "You have chosen Rock --- you Lost!!!"
-}else if(computerSelection === "Paper" && playerSelection === "Scissors"){
-return "You have chosen Scissors --- You Won"    
-}else if(computerSelection === "Scissors" && playerSelection === "Paper"){
-return "You have chosen Paper --- You Lost!!!"
-}else if(computerSelection === "Scissors" && playerSelection === "Rock"){
-return "You chose Rock --- You won!!!"
+    if (computerSelection === "Rock" && playerSelection === "Paper"){
+    return "You have chosen Paper---You Wonn!!" 
+    }else if (computerSelection === "Rock" && playerSelection === "Scissors"){
+    return "You have chosen Scissors---You Lost!!"
+    }else if (computerSelection === "Rock" && playerSelection === "Rock" || computerSelection === "Scissors" && playerSelection === "Scissors" || computerSelection === "Paper" && playerSelection === "Paper"){
+    return `You have chosen ${playerSelection} --- you Tie!!`
+    }else if(computerSelection === "Paper" && playerSelection === "Rock"){
+    return "You have chosen Rock --- you Lost!!!"
+    }else if(computerSelection === "Paper" && playerSelection === "Scissors"){
+    return "You have chosen Scissors --- You Won"    
+    }else if(computerSelection === "Scissors" && playerSelection === "Paper"){
+    return "You have chosen Paper --- You Lost!!!"
+    }else if(computerSelection === "Scissors" && playerSelection === "Rock"){
+    return "You chose Rock --- You won!!!"
+    
+    }
+    }
 
-}
-}
+// // getComputerChoice function
 
 function getComputerChoice(){
   
@@ -69,6 +51,23 @@ function getComputerChoice(){
       
 }
 
-const playerSelection = "Rock";
+const playerSelection = ""
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+
+// console.log(playRound(playerSelection, computerSelection))
+
+
+// game function
+
+var i = 0
+function game(){
+    while(i<5){
+        const playerSelection = "Rock";
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+        i++
+    }
+}
+
+game()
+
